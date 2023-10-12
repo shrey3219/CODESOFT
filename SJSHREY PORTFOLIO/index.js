0c74ea74f1f493d248e7d8a728a666417bae7475
shrey3219 <130434847+shrey3219@users.noreply.github.com>
2023-10-12 14:09:10 +0530

    Add files via upload

---
+++ SJSHREY PORTFOLIO/index.js
@@ -0,0 +1,79 @@
+ /*-------------------------TOGGLING HAMBURGER-----------------------------------------*/
+let menuIcon= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
+menuIcon.onclick = () => { 
+    navbar.classList.toggle('active');
+}
+
+ /*-------------------------CLICKING ON NAV LINKS FOR SCROLLING-----------------------------------------*/
+let sections= document.querySelectorAll('section');
+let navLinks= document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top= window.scrollY;
+        let offset= sec.offsetTop - 150;
+        let height= sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id+ ']').classList.add('active');
+             });
+        };
+    });
+
+ /*-------------------------REMOVE TOGGLING WHEN CLICK ON HUMBURGER-----------------------------------------*/
+    navbar.classList.remove('active');
+};
+
+// let header=document.querySelector('header');
+// header.classList.toggle('sticky', window.scrollY >100);
+
+ /*-------------------------ANIMATION THROUGH SCROLL REVEAL-----------------------------------------*/
+ScrollReveal({
+    // reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img,.servcontainer,.portbox,.contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-more',{origin:'right'});
+
+ /*-------------------------TYPED FROM CDN LINK -----------------------------------------*/  
+const typed= new Typed('.mutiple-text',{
+    strings:['Frontend Developer','Frontend Developer','Frontend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
+
+
+
+ /*-------------------------SENDEMAIL BY ELASTIC MAIL-----------------------------------------*/
+ function sendMail(){
+     var params={
+        user_name : document.getElementById("Full Name").value,
+        user_email : document.getElementById("email").value,
+        message : document.getElementById("messagess").value,
+     };
+    
+     emailjs.send("service_kayn97w", "template_fkb4v2r",params)
+     .then(
+        res =>{
+            document.getElementById("Full Name").value  = "";
+            document.getElementById("email").value = "";
+            document.getElementById("messagess").value = "";
+            console.log(res);
+            alert("SUCCESS!");
+        } )
+    .catch((err) => console.log(err));
+      
+    }
+
